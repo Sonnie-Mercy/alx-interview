3c1f99d022d3ef6812bfaff33e4d02bf9cf9300e
Mercy Muthoni <ndirangumercy4@gmail.com>
2024-03-28 12:54:42 +0000

    log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+The log Parsing task - printing as per requiremens
+"""
+from sys import stdin
+
+
+def print_stats(total_size, status_codes):
+    """ This prints"""
+    print("File size: " + str(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(code + ": " + str(status_codes[code]))
+
+
+line_num = 0
+total_size = 0
+status_code = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+
+try:
+    for line in stdin:
+        line_num += 1
+        split_line = line.split()
+
+        if len(split_line) > 1:
+            total_size += int(split_line[-1])
+
+        if len(split_line) > 2 and split_line[-2].isnumeric():
+            status_code = split_line[-2]
+        else:
+            status_code = 0
+
+        if status_code in status_codes.keys():
+            status_codes[status_code] += 1
+
+        if line_num % 10 == 0:
+            print_stats(total_size, status_codes)
+
+    print_stats(total_size, status_codes)
+
+except (KeyboardInterrupt):
+    raise
