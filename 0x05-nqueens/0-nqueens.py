33885472596eb096a69025768d88d0f67628a23d
Mercy Muthoni <ndirangumercy4@gmail.com>
2024-07-04 10:51:01 +0000

    n queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+n queens interview
+"""
+import sys
+
+def is_safe(board, row, col, N):
+    """
+    the is_safe function
+    """
+    # Check this column on upper side
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check upper diagonal on right side
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_nqueens(board, row, N, solutions):
+    if row >= N:
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            solve_nqueens(board, row + 1, N, solutions)
+            board[row][col] = 0  # Backtrack
+
+def print_solutions(solutions):
+    for solution in solutions:
+        print(solution)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0] * N for _ in range(N)]
+    solutions = []
+    solve_nqueens(board, 0, N, solutions)
+    print_solutions(solutions)
+
+if __name__ == "__main__":
+    main()
